6fbe2e7d3a61add259e14cef88d415fa96704dc3
Jack Butler <jacknbutler@outlook.com>
2023-02-11 23:00:01 +0000

    User class and tests

--- src/User.js
+++ src/User.js
@@ -1,5 +1,18 @@
 class User {
-  // User code here
+  constructor(name, password, age, loggedIn = false){
+    this.name = name;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = loggedIn;
+  }
+  login(password){
+    if (password == this.password){
+      this.loggedIn = true;
+    }
+  }
+  logout(){
+    this.loggedIn = false;
+  }
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,7 +1,35 @@
 const User = require('../src/User')
 
 // User tests here
-
+describe ('User', function () {
+    test ('test username is valid', function(){
+        const User1 = new User('Adam');
+        expect(typeof User1.name).toBe('string');
+    })
+    test ('test password is valid', function(){
+        const User2 = new User('Dave', 'securepass');
+        expect(typeof User2.password).toBe('string');
+    })
+    test ('age is valid and meets requirements', function(){
+        const User3 = new User('Sarah', 'spass', 18);
+        expect(typeof User3.age).toBe('number');
+        expect(User3.age >= 18).toBe(true);
+    })
+    test ('check login function works', function(){
+        const User4 = new User('Alan', 'alanpass', 25);
+        expect(User4.loggedIn == false).toBe(true);
+        User4.login('wrongpass');
+        expect(User4.loggedIn == false).toBe(true);
+        User4.login('alanpass');
+        expect(User4.loggedIn == true).toBe(true);
+    })
+    test ('test logout function works', function(){
+        const User5 = new User('Susan', 'susanpass', 94);
+        User5.login('susanpass');
+        User5.logout();
+        expect(User5.loggedIn == true).toBe(false);
+    })
+})
 // test username
 
 // test password
